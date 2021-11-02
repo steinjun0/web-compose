8fea0043b77e91d45f0c4ca5703b56767058112c
steinjun0 <1142308@naver.com>
2021-11-02 15:23:20 +0900

    .py를 post하여 연산결과를 return 받기 - post form-data, key: value로 전달

--- django/webcompose/blog/views.py
+++ django/webcompose/blog/views.py
@@ -10,11 +10,17 @@ class test(APIView):
     authentication_classes = []
     permission_classes = []
     
-    def get(self, request):
+    def post(self, request, **kargs):
+        # print(request.data['value'])
+        # print(request.data['value'].read())
         # f = open('./temp.py', 'w')
         # f.write("print('hello')")
         # f.close()
         # exec(open('./temp.py').read())
         from . import temp
-        result = temp.some_func()
-        return Response(result, status=status.HTTP_200_OK)
+        result = ''
+        loc = {}
+        exec(request.data['value'].read(), globals(), loc)
+        return_workaround = loc['result']
+        print(return_workaround)
+        return Response(return_workaround, status=status.HTTP_200_OK)
