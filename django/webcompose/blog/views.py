6ca2b658e7234066a2605c86fe2d46f82fb29cd1
steinjun0 <1142308@naver.com>
2021-11-02 12:30:17 +0900

    django test용 urls 추가 및 drf 추가 설정

--- django/webcompose/blog/views.py
+++ django/webcompose/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+class test(APIView):
+    # def get_queryset(self):
+    #     return 'hello'
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        return Response('hi', status=status.HTTP_200_OK)
